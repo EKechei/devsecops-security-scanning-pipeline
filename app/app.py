c1236043289e036270c09e98e6b6b25354afbc8c
Edith Cherotich <128794751+EKechei@users.noreply.github.com>
2025-12-15 14:28:41 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Edith's DevOps pipeline Demo on AWS EKS!"
+    return "Hello DevSecOps!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
